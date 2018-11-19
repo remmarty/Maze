76d30854cfe26b65342a1b1fadd69f39a1bafcf0
Unknown <remi.martyniak@linux.pl>
2018-11-19 21:34:40 +0100

    Check for collisions. Introduce EXIT_AREA (7).

--- main.cpp
+++ main.cpp
@@ -13,17 +13,19 @@ struct MapElement {
 	static const char EMPTY = ' '; 
 	static const char PLAYER = 'X';
 	static const char FOG = 177; // ▒
+	static const char EXIT_AREA = 'W';
 };
 
 struct Symbols {
 
+	static const int EMPTY = 0;
 	static const int VERTICAL = 1;
 	static const int HORIZONTAL = 2;
 	static const int UPPER_RIGHT_CORNER = 3;
 	static const int UPPER_LEFT_CORNER = 4;
 	static const int BOTTOM_RIGHT_CORNER = 5;
 	static const int BOTTOM_LEFT_CORNER = 6;
-	static const int EMPTY = 0;
+	static const int EXIT_AREA = 7;
 	static const int PLAYER = 9;
 
 };
@@ -62,10 +64,24 @@ struct Player {
 	int x = 5;
 	int y = 5;
 
-	void move(int vertical_step, int horizontal_step){
-		y += vertical_step;
-		x += horizontal_step;
+	void move(int vertical_step, int horizontal_step, int map[Settings::MAP_HEIGHT][Settings::MAP_WIDTH]){
+		// Pretend this will be our final position
+		int tmp_y = y + vertical_step;
+		int tmp_x = x + horizontal_step;
+
+		if (map[tmp_y][tmp_x] == Symbols::EXIT_AREA) {
+			std::cout << "You win!" << std::endl;
+			exit(0);
+		} else if (map[tmp_y][tmp_x] == Symbols::EMPTY) {
+			y += vertical_step;
+			x += horizontal_step;
+		}
+		else {
+			std::cout << "Illegal move!" << std::endl;
+		}
+		
 	}
+
 };
 
 struct GameEngine {
@@ -82,13 +98,13 @@ struct GameEngine {
 	void triggerActions(char key) {
 		/* Call behaviour depending on keyboard input */
 		if (key == Settings::KEY_PLAYER_UP)
-			player.move(-1, 0);
+			player.move(-1, 0, level.map);
 		else if (key == Settings::KEY_PLAYER_DOWN)
-			player.move(1, 0);
+			player.move(1, 0, level.map);
 		else if (key == Settings::KEY_PLAYER_LEFT)
-			player.move(0, -1);
+			player.move(0, -1, level.map);
 		else if (key == Settings::KEY_PLAYER_RIGHT)
-			player.move(0, 1);
+			player.move(0, 1, level.map);
 		else
 			// TODO
 			std::cout << "Key not handled TODO" << std::endl;
@@ -160,6 +176,10 @@ struct GameEngine {
 
 						case Symbols::BOTTOM_LEFT_CORNER:
 							std::cout << MapElement::BOTTOM_LEFT_CORNER;
+							break;
+
+						case Symbols::EXIT_AREA:
+							std::cout << MapElement::EXIT_AREA;
 							break;
 
 						default:
